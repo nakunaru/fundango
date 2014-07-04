c1d084d1876e3b4166cb7d4b30a283590ee1ede7
karamage <karamage.umauma@gmail.com>
2014-07-05 08:32:58 +0900

    twitter login

--- fuel/app/classes/controller/twitterlogin.php
+++ fuel/app/classes/controller/twitterlogin.php
@@ -4,6 +4,10 @@ class Controller_Twitterlogin extends Controller
 {
     public function action_index()
     {
+        if(!Twitter::logged_in()) {
+            Twitter::set_callback(Uri::current());  // 戻り先 URL
+            Twitter::login();
+        }
         $data = array();
         //$data['users'] = Model_Tb1::find_all();
         //文字列を返す
