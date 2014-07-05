555619ac6931cb0bd7a238f1637cfa090a0524c7
karamage <karamage.umauma@gmail.com>
2014-07-05 18:21:22 +0900

    home.php filepath update

--- fuel/app/views/scds/home.php
+++ fuel/app/views/scds/home.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html> 
 <html>
 <head>
-<meta charset="UTF-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1" />
-<title>W@tchMe! Home</title>
-<link rel="stylesheet" href="themes/watchme.min.css" />
-<link rel="stylesheet" href="themes/jquery.mobile.icons.min.css" />
-<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.2/jquery.mobile.structure-1.4.2.min.css" />
-<link rel="stylesheet" href="css/sp.css" />
-<script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
-<script src="http://code.jquery.com/mobile/1.4.2/jquery.mobile-1.4.2.min.js"></script> 
-<script type="text/javascript" src="js/spinit.js"></script>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>W@tchMe! Home</title>
+    <?php echo Asset::css('themes/watchme.min.css'); ?>
+    <?php echo Asset::css('themes/jquery.mobile.icons.min.css'); ?>
+    <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.2/jquery.mobile.structure-1.4.2.min.css" />
+    <?php echo Asset::css('sp.css'); ?>
+    <script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="http://code.jquery.com/mobile/1.4.2/jquery.mobile-1.4.2.min.js"></script>
+    <?php echo Asset::js('spinit.js'); ?>
 </head>
 <body>
 <!--ページ領域-->
