014e6fac59bc3640b7fbc378210f10e01cb6a19e
karamage <karamage.umauma@gmail.com>
2014-07-05 18:48:51 +0900

    ajax false twitterlogin update

--- fuel/app/classes/controller/twitterlogin.php
+++ fuel/app/classes/controller/twitterlogin.php
@@ -2,6 +2,7 @@
 
 class Controller_Twitterlogin extends Controller
 {
+    /*
     public function action_index()
     {
         if(!Twitter::logged_in()) {
@@ -14,5 +15,51 @@ class Controller_Twitterlogin extends Controller
         //$data['users'] = Model_Tb1::find_all();
         //文字列を返す
         return Response::forge(View::forge('scds/home', $data));
+    }
+    */
+    public function action_login()
+    {
+        if ( ! Twitter::logged_in() )
+        {
+            Twitter::set_callback(Uri::create('twitter/callback'));
+            Twitter::login();
+        }
+        else
+        {
+            Response::redirect(Uri::create('/'));
+        }
+    }
+
+    public function action_logout()
+    {
+        Session::destroy();
+        Response::redirect(Uri::create('/'));
+    }
+
+    public function action_callback()
+    {
+        $tokens = Twitter::get_tokens();
+        $twitter_user = Twitter::get('account/verify_credentials');
+
+        // Update or create the user.  We update every time a user logs in
+        // so that if they update their profile, we get that update.
+        /*
+        $user = Model_User::find_by_screen_name($twitter_user->screen_name);
+        if ( ! $user)
+        {
+            $user = new Model_User();
+        }
+        $user->screen_name = $twitter_user->screen_name;
+        $user->name = $twitter_user->name;
+        $user->description = $twitter_user->description;
+        $user->avatar = $twitter_user->profile_image_url;
+        $user->oauth_token = $tokens['oauth_token'];
+        $user->oauth_token_secret = $tokens['oauth_token_secret'];
+        $user->save();
+        */
+
+        Session::set('user_id', $user->id);
+
+        Response::redirect(Uri::create('/'));
     }
 }
