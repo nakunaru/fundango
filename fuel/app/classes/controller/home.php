97e7c165d5358cc0f69c08c4ff883d7108ae5335
karamage <karamage.umauma@gmail.com>
2014-07-07 07:22:21 +0900

    security update twitter timeline get

--- fuel/app/classes/controller/home.php
+++ fuel/app/classes/controller/home.php
@@ -2,16 +2,30 @@
 
 class Controller_Home extends Controller
 {
-  public function action_index()
-  {
-      $data = array();
-      $twitter_user = Twitter::get('account/verify_credentials');
-      $user = Model_User::find_one_by('screen_name', $twitter_user->screen_name, '=');
-      $timeline = Twitter::get("statuses/home_timeline");
-      $data['user'] = $user;
-      $data['timeline'] = $timeline;
-      //$output = print_r($timeline,true);
-      //Log::debug('timeline = ' . $output);
-      return Response::forge(View::forge('scds/home', $data));
-  }
+    /**
+     * あなたの before メソッド
+     */
+    public function before()
+    {
+        parent::before(); // この行がないと、テンプレートが動作しません!
+
+        // 何かする
+        if ( ! Twitter::logged_in() )
+        {
+            Response::redirect(Uri::create('login'));
+        }
+    }
+
+    public function action_index()
+    {
+        $data = array();
+        $twitter_user = Twitter::get('account/verify_credentials');
+        $user = Model_User::find_one_by('screen_name', $twitter_user->screen_name, '=');
+        $timeline = Twitter::get("statuses/home_timeline");
+        $data['user'] = $user;
+        $data['timeline'] = $timeline;
+        //$output = print_r($timeline,true);
+        //Log::debug('timeline = ' . $output);
+        return Response::forge(View::forge('scds/home', $data));
+    }
 }
