bd0318b6513eb40edc32e7b53a83321fc81fcb94
karamage <karamage.umauma@gmail.com>
2014-07-06 20:04:08 +0900

    security update twitter timeline get

--- fuel/app/views/scds/home.php
+++ fuel/app/views/scds/home.php
@@ -29,6 +29,8 @@
       <?php echo $user['screen_name'] ?><br>
       <?php echo $user['followers_count'] ?><br>
       <?php echo count($timeline) ?><br>
+      <?php echo count($timeline['data']) ?><br>
+      <?php echo $timeline['data'][0]->text ?><br>
   </div>
 
   <div data-role="footer" data-position="fixed">
