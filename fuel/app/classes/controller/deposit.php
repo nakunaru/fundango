01728e6a054a9b8e84fe2911c9f818ac3980b917
karamage <karamage.umauma@gmail.com>
2014-07-09 08:21:51 +0900

    setting and friend panel open

---
+++ fuel/app/classes/controller/deposit.php
@@ -0,0 +1,57 @@
+<?php
+
+class Controller_Deposit extends Controller
+{
+    /**
+     * あなたの before メソッド
+     */
+    public function before()
+    {
+        parent::before(); // この行がないと、テンプレートが動作しません!
+
+        // 何かする
+        if ( ! Twitter::logged_in() )
+        {
+            Response::redirect(Uri::create('login'));
+        }
+    }
+
+    public function action_index()
+    {
+        /*
+        $data = array();
+        $twitter_user = Twitter::get('account/verify_credentials');
+        $user = Model_User::find_one_by('screen_name', $twitter_user->screen_name, '=');
+        $timeline = Twitter::get("statuses/home_timeline");
+        $ids = Twitter::get("followers/ids");
+        $data['user'] = $user;
+        $data['timeline'] = $timeline->__resp->data;
+        $data['ids'] = $ids->__resp->data->ids;
+        $idstr = '';
+        $count = 0;
+        foreach ($data['ids'] as $id){
+            if ($count == 100) {
+               break;
+            }
+            if ($idstr == '') {
+                $idstr = $id;
+            } else {
+                $idstr = $idstr . ',' . $id;
+            }
+            $count++;
+        }
+        //Log::warning('idstr = ' . $idstr);
+        $followers = Twitter::get("users/lookup",array('user_id'=>$idstr));
+        $data['followers'] = $followers->__resp->data;
+        //$output = print_r($followers,true);
+        //Log::warning('followers = ' . $output);
+        return Response::forge(View::forge('scds/home', $data));
+        */
+    }
+
+    public function action_add()
+    {
+        $data = array();
+        return Response::forge(View::forge('scds/deposit', $data));
+    }
+}
---
+++ fuel/app/views/scds/deposit.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html> 
+<html>
+<head>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<title>W@tchMe! Login</title>
+<?php echo Asset::css('themes/watchme.min.css'); ?>
+<?php echo Asset::css('themes/jquery.mobile.icons.min.css'); ?>
+<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.2/jquery.mobile.structure-1.4.2.min.css" />
+<?php echo Asset::css('sp.css'); ?>
+<script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+<script src="http://code.jquery.com/mobile/1.4.2/jquery.mobile-1.4.2.min.js"></script> 
+<?php echo Asset::js('spinit.js'); ?>
+</head>
+<body>
+<!--ページ領域-->
+<div data-role="page" id="deposit"  data-title="deposit">
+
+</div>
+</body>
+</html>
