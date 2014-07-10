df4d9ebcd6f20064994bc86254c23a5148ca22da
karamage <karamage.umauma@gmail.com>
2014-07-10 20:55:05 +0900

    image diet

--- fuel/app/views/scds/login.php
+++ fuel/app/views/scds/login.php
@@ -36,7 +36,7 @@
     <br>
     <br>
     <!-- center><img id="logintitle"  src="../../images/title.jpg"></center -->
-    <center><?php echo Asset::img('fundango_logo.jpg',array('id'=>'logintitle'));  ?></center>
+    <center><?php echo Asset::img('fundango_logo_mini.jpg',array('id'=>'logintitle'));  ?></center>
     <div id="sociallogincontain" data-role="fieldcontain">
         <div data-role="fieldcontain">
             <!-- a href="home.html" data-icon="twittericon" data-role="button" id="tweetloginbtn">Twitterでログイン</a -->
