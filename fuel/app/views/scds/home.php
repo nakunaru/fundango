dc4dd324269ff833c3886150c243af5578866848
karamage <karamage.umauma@gmail.com>
2014-07-11 07:42:16 +0900

    header update

--- fuel/app/views/scds/home.php
+++ fuel/app/views/scds/home.php
@@ -54,9 +54,9 @@
         </ul>
     </div>
     <div data-role="footer" data-position="fixed">
-        <div data-role="navbar">
+        <div data-role="navbar" data-iconpos="left">
             <ul>
-                <li><a href="#" data-icon="home">ホーム</a></li>
+                <li><a href="#" class="ui-btn-active" data-icon="home">ホーム</a></li>
                 <li><a href="#" data-icon="deposit">デポジット</a></li>
                 <li><a href="#" data-icon="notify">お知らせ</a></li>
             </ul>
