c32f8c36d4daeb5642adf565b7caa2fa46bab6a4
karamage <karamage.umauma@gmail.com>
2014-07-12 15:47:57 +0900

    port4lio model add

--- fuel/app/classes/controller/deposit.php
+++ fuel/app/classes/controller/deposit.php
@@ -18,40 +18,18 @@ class Controller_Deposit extends Controller
 
     public function action_index()
     {
-        /*
-        $data = array();
-        $twitter_user = Twitter::get('account/verify_credentials');
-        $user = Model_User::find_one_by('screen_name', $twitter_user->screen_name, '=');
-        $timeline = Twitter::get("statuses/home_timeline");
-        $ids = Twitter::get("followers/ids");
-        $data['user'] = $user;
-        $data['timeline'] = $timeline->__resp->data;
-        $data['ids'] = $ids->__resp->data->ids;
-        $idstr = '';
-        $count = 0;
-        foreach ($data['ids'] as $id){
-            if ($count == 100) {
-               break;
-            }
-            if ($idstr == '') {
-                $idstr = $id;
-            } else {
-                $idstr = $idstr . ',' . $id;
-            }
-            $count++;
-        }
-        //Log::warning('idstr = ' . $idstr);
-        $followers = Twitter::get("users/lookup",array('user_id'=>$idstr));
-        $data['followers'] = $followers->__resp->data;
-        //$output = print_r($followers,true);
-        //Log::warning('followers = ' . $output);
-        return Response::forge(View::forge('scds/home', $data));
-        */
     }
 
+    /**
+     * 団子を友達にデポジットする
+     * @return mixed
+     */
     public function action_add()
     {
+        //ホーム画面のデータを取得する
         $data = Homecommon::getdata();
+
+        //ポートフォリオにデータを追加する
         return Response::forge(View::forge('scds/home', $data));
     }
 }
---
+++ fuel/app/classes/model/port4lio.php
@@ -0,0 +1,5 @@
+<?php
+class Model_Port4lio extends \Model_Crud{
+    protected static $_table_name = 'port4lio';
+    protected static $_primary_key = 'id';
+}
