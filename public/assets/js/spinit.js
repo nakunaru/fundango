f5c5ad17c9d100fd64be8dc4b50ec081a70976e3
karamage <karamage.umauma@gmail.com>
2014-07-13 10:52:07 +0900

    deposit add control

--- public/assets/js/spinit.js
+++ public/assets/js/spinit.js
@@ -21,7 +21,7 @@ $(document).on( "pageinit", "#login", function( event ) {
 });
 
 //$(document).on( "pageinit", "#home", function( event ) {
-$(document).on( "pageinit", ".ui-page-active #home", function( event ) {
+$(document).on( "pageshow", "#home", function( event ) {
     var aaa = "";
     /*
     $( "#settingopenbtn" ).bind( "click", function(event, ui) {
