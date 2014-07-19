358c1d4d294ffc01b01991090d3d011b184ddff8
karamage <karamage.umauma@gmail.com>
2014-07-19 12:44:33 +0900

    deposit view add

--- fuel/app/views/scds/footer.php
+++ fuel/app/views/scds/footer.php
@@ -2,7 +2,7 @@
     <div data-role="navbar" data-iconpos="left">
         <ul>
             <li><a href="<?php echo URI::create('home'); ?>" class="ui-btn-active" data-icon="home">ホーム</a></li>
-            <li><a href="<?php echo URI::create('deposit'); ?>" data-icon="deposit" data-ajax="false">デポジット</a></li>
+            <li><a href="<?php echo URI::create('deposit'); ?>" data-icon="deposit">デポジット</a></li>
             <li><a href="<?php echo URI::create('notify'); ?>" data-icon="notify">お知らせ</a></li>
         </ul>
     </div>
