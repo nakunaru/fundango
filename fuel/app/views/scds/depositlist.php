6da9aff2cf163eb61118969384aab04650ede876
karamage <karamage.umauma@gmail.com>
2014-07-19 14:29:10 +0900

    deposit view tab add

--- fuel/app/views/scds/depositlist.php
+++ fuel/app/views/scds/depositlist.php
@@ -1,11 +1,26 @@
 <div role="main" class="ui-content">
-    <ul data-role="listview" data-filter="true" data-filter-placeholder="Search Portfolio...">
-        <?php
-        echo '<li data-role="list-divider">ポートフォリオ</li>';
-        foreach ($port4lio as $data)
-        {
-            echo '<li>'.'<div style="text-overflow:ellipsis; overflow:hidden; white-space: normal;">' . $data->to_screen_name . '</div>' . '<p style="text-overflow:ellipsis; overflow:hidden; ">' . $data->depositnum .'</p></li>';
-        }
-        ?>
-    </ul>
+    <!--3タブパネル全体を定義-->
+    <div id="tabs" data-role="tabs">
+        <!--2タブリストを準備-->
+        <div data-role="navbar">
+            <ul>
+                <li><a href="#tabport4lio" class="ui-btn-active">ポートフォリオ</a></li>
+                <li><a href="#tabdeposited">被デポジットリスト</a></li>
+            </ul>
+        </div>
+        <!--1パネル（コンテンツ領域）を準備-->
+        <div id="tabport4lio" class="ui-body ui-body-a">
+            <ul data-role="listview" data-filter="true" data-filter-placeholder="Search Portfolio...">
+                <?php
+                echo '<li data-role="list-divider">ポートフォリオ</li>';
+                foreach ($port4lio as $data)
+                {
+                    echo '<li>'.'<div style="text-overflow:ellipsis; overflow:hidden; white-space: normal;">' . $data->to_screen_name . ' さんにデポジットしました</div>' . '<p style="text-overflow:ellipsis; overflow:hidden; ">CG:xxd</p><div class="ui-li-count">' . $data->depositnum .'d</div></li>';
+                }
+                ?>
+            </ul>
+        </div>
+        <div id="tabdeposited" class="ui-body ui-body-a">
+        </div>
+    </div>
 </div>
