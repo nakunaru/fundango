7a8126a3a988a50c2c2151b2f508059460bb592f
karamage <karamage.umauma@gmail.com>
2014-07-22 07:36:11 +0900

    board view add

--- fuel/app/classes/boardcommon.php
+++ fuel/app/classes/boardcommon.php
@@ -9,21 +9,21 @@ class Boardcommon {
     public static function getdata()
     {
         $data = array();
-        /*
         $user = Session::get('user');
         //$followers = Session::get('followers');
         $followers = array();
         //$output = print_r($user,true);
         //Log::warning('session data = ' . $output);
         //$user = $data['user'];
+        /*
         $tuserid = $user->tuserid;
         $port4lio = Model_Port4lio::find_by('from_tuserid', $tuserid, '=');
         $depositedlist = Model_Port4lio::find_by('to_tuserid', $tuserid, '=');
+        */
         $data['user'] = $user;
         $data['followers'] = $followers;
-        $data['port4lio'] = $port4lio;
-        $data['depositedlist'] = $depositedlist;
-        */
+        //$data['port4lio'] = $port4lio;
+        //$data['depositedlist'] = $depositedlist;
         return $data;
     }
 
@@ -31,9 +31,9 @@ class Boardcommon {
     {
         $data = Boardcommon::getdata();
         $view = View::forge('scds/board', $data);
-        /*
         $view->set_global('user', $data['user']);
         $view->set_global('followers', $data['followers']);
+        /*
         $view->set_global('port4lio', $data['port4lio']);
         $view->set_global('depositedlist', $data['depositedlist']);
         */
