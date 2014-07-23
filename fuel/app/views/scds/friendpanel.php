a2a094e45f897b945518ca0aad3941b217579067
karamage <karamage.umauma@gmail.com>
2014-07-23 22:16:45 +0900

    followers image

--- fuel/app/views/scds/friendpanel.php
+++ fuel/app/views/scds/friendpanel.php
@@ -5,7 +5,7 @@
         echo '<li data-role="list-divider">友達</li>';
         foreach ($followers as $data)
         {
-            echo '<li class="friendli" screen_name="' . $data->screen_name . '" tuserid="' . $data->id . '"><a href="#depositAddDialog" data-rel="dialog" data-transition="pop"><div style="text-overflow:ellipsis; overflow:hidden; white-space: normal;">' . $data->name . '</div><div class="ui-li-count">0d</div><p style="text-overflow:ellipsis; overflow:hidden; ">' . $data->screen_name . '</p></a></li>';
+            echo '<li class="friendli" screen_name="' . $data->screen_name . '" tuserid="' . $data->id . '"><a href="#depositAddDialog" data-rel="dialog" data-transition="pop">'. '<img src="' . $data->profile_image_url . '">' . '<div style="text-overflow:ellipsis; overflow:hidden; white-space: normal;">' . $data->name . '</div><div class="ui-li-count">0d</div><p style="text-overflow:ellipsis; overflow:hidden; ">' . $data->screen_name . '</p></a></li>';
         }
         ?>
     </ul>
