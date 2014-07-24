cad884f09d8fd1ddbf615c4e30e287e556afe21a
karamage <karamage.umauma@gmail.com>
2014-07-25 07:44:43 +0900

    wow animate add

--- fuel/app/views/scds/htmlheader.php
+++ fuel/app/views/scds/htmlheader.php
@@ -8,5 +8,5 @@
 <script type="text/javascript" src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
 <script type="text/javascript" src="http://code.jquery.com/mobile/1.4.2/jquery.mobile-1.4.2.min.js"></script>
 <?php echo Asset::js('Chart.min.js'); ?>
-<?php echo Asset::js('wow.min.js'); ?>
+<?php echo Asset::js('wow.js'); ?>
 <?php echo Asset::js('spinit.js'); ?>
