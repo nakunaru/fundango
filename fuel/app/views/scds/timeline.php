bc888b8f7017db5324ed68f6439c605537c0a4e7
karamage <karamage.umauma@gmail.com>
2014-07-24 21:42:45 +0900

    timmeline image

--- fuel/app/views/scds/timeline.php
+++ fuel/app/views/scds/timeline.php
@@ -8,8 +8,10 @@
                 . '"><div class="timelinetext" style="text-overflow:ellipsis; overflow:hidden; white-space: normal;">'
                 . $data->text . '</div>' . '<p style="text-overflow:ellipsis; overflow:hidden; ">'
                 . $data->user->name . ' @' . $data->user->screen_name .'</p>';
-            if (count($data->entities->media) > 0) {
-                echo 'media';
+            if (property_exists($data->entities,'media')) {
+                if (count($data->entities->media) > 0) {
+                    echo 'media';
+                }
             }
             if (count($data->entities->urls) > 0) {
                 echo 'urls';
