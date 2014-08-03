207940df4ba7709a6354c63f0685dc3b5efe574c
karamage <karamage.umauma@gmail.com>
2014-08-03 11:03:19 +0900

    friend panel social credit

--- fuel/app/classes/homecommon.php
+++ fuel/app/classes/homecommon.php
@@ -76,10 +76,13 @@ class Homecommon {
             if (isset($to_users[$followerid])) {
                 $to_user = $to_users[$followerid];
                 if ($to_user) {
+                    /*
                     $follower->credit = $to_user->social_credit;
                     if ($to_user->deposited_credit) {
                         $follower->credit = $to_user->social_credit + $to_user->deposited_credit;
                     }
+                    */
+                    $follower->credit = 0;
                 } else {
                     $follower->credit = 0;
                 }
