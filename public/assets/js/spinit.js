c4e7c04bd9cd0e13bd030598bae3b233285132d8
karamage <karamage.umauma@gmail.com>
2014-08-04 08:18:27 +0900

    timeline get dango and date

--- public/assets/js/spinit.js
+++ public/assets/js/spinit.js
@@ -264,8 +264,9 @@ function getTimeline()
                     + '<img class="slideRight" src="' + $data.user.profile_image_url
                     + '"><div class="timelinetext slideLeft" style="text-overflow:ellipsis; overflow:hidden; white-space: normal; width: 80%">'
                     + $data.text + '</div>' + '<p style="text-overflow:ellipsis; overflow:hidden; ">'
-                    + '<p class="ui-li-aside">2014/08/xx</p>'
-                    + $data.user.name + ' @' + $data.user.screen_name + ' (0d)</p>'
+                    + '<p class="ui-li-aside">10:01</p>'
+                    + $data.user.name + ' @' + $data.user.screen_name + '</p>'
+                    + '<p class="ui-li-count">0d</p>'
                     //+ '<a href="#">デポる</a>'
                     ;
                 //$(timelineul).prepend(str);
