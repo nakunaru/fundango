59597b87f4245aef8f678ca307db3be4c6088811
karamage <karamage.umauma@gmail.com>
2014-08-22 07:47:14 +0900

    infobutton

--- fuel/app/views/scds/header.php
+++ fuel/app/views/scds/header.php
@@ -1,5 +1,5 @@
 <div data-role="header" data-position="fixed">
-    <a href="#" data-icon="gear" id="settingopenbtn">設定</a>
+    <a href="#" data-icon="gear" id="settingopenbtn">情報</a>
     <h1><?php echo $user['screen_name'] .'(' .(0 + $user['social_credit'] + $user['deposited_credit']) . 'd)' ?> さんのホーム</h1>
     <a href="#" data-icon="friend" id="friendopenbtn">友達</a>
 </div>
