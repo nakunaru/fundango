aabca65454c57c74e8aca13a4d12647abf35482b
karamage <karamage.umauma@gmail.com>
2014-08-23 15:21:17 +0900

    get followers id save

--- fuel/app/classes/homecommon.php
+++ fuel/app/classes/homecommon.php
@@ -137,10 +137,10 @@ class Homecommon {
             $output = print_r($ret,true);
             Log::warning('follower ids session all get ret = ' . $output);
             */
-            Log::warning('ids get twitter');
+            //Log::warning('ids get twitter');
         } else {
             $ids = explode(',', $idstr);
-            Log::warning('ids get cache');
+            //Log::warning('ids get cache');
         }
         return $ids;
     }
