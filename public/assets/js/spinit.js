2a74e217ae38a67a3adb830687230da01b959074
karamage <karamage.umauma@gmail.com>
2014-08-27 21:53:27 +0900

    disabled deposit in 0d

--- public/assets/js/spinit.js
+++ public/assets/js/spinit.js
@@ -199,7 +199,8 @@ $(document).on( "pageshow", "#home", function( event ) {
         $('#todepoimg').attr('src', $(this).attr('image_url'));
         $('#to_image_url').val($(this).attr('image_url'));
         var enabled_deponum = $('#account_enabled_deposit_credit').attr('credit');
-        if (enabled_deponum == '0') {
+        enabled_deponum = 0 + enabled_deponum;
+        if (enabled_deponum <= '0') {
             alert('デポジット可能な団子がありません＞＜');
             //$('#depositAddDialog')
             $('.ui-dialog').dialog('close');
