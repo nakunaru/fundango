dcecc147575cbeed9d9cb9ff0f9503d30a1b2aab
karamage <karamage.umauma@gmail.com>
2014-09-06 10:12:09 +0900

    header dango display update

--- fuel/app/views/scds/header.php
+++ fuel/app/views/scds/header.php
@@ -1,6 +1,6 @@
 <div data-role="header" data-position="fixed">
     <!-- data-icon="gear" data-icon="friend" -->
     <a href="#" id="settingopenbtn">情報</a>
-    <h1><?php echo $user['screen_name'] .'(' .(0 + $user['social_credit'] + $user['deposited_credit']) . 'd)' ?> さんのホーム</h1>
+    <h1><?php echo '(' .(0 + $user['social_credit'] + $user['deposited_credit']) . 'd)' . $user['screen_name'] ?> さんのホーム</h1>
     <a href="#" id="friendopenbtn">友達</a>
 </div>
