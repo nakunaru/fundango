671fc778dcf8c46bff0e7f52c2c872b6736f7fdd
karamage <karamage.umauma@gmail.com>
2014-09-11 07:48:27 +0900

    google analytics header add

--- fuel/app/views/scds/htmlheader.php
+++ fuel/app/views/scds/htmlheader.php
@@ -10,3 +10,13 @@
 <?php echo Asset::js('Chart.min.js'); ?>
 <!-- ?php echo Asset::js('wow.js'); ? -->
 <?php echo Asset::js('spinit.js'); ?>
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-23784522-2', 'auto');
+    ga('send', 'pageview');
+
+</script>
