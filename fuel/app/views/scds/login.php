82773ec9fc1ddf2e4eb1e7c5747a6fd608316c78
karamage <karamage.umauma@gmail.com>
2014-09-15 09:54:43 +0900

    favicon set

--- fuel/app/views/scds/login.php
+++ fuel/app/views/scds/login.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="shortcut icon" href="<?php echo Asset::get_file('favicon.ico'); ?>" >
+    <link rel="shortcut icon" href="<?php echo Asset::get_file('favicon.ico', 'img'); ?>" >
     <title>fundango</title>
     <?php echo Asset::css('themes/watchme.min.css'); ?>
     <?php echo Asset::css('themes/jquery.mobile.icons.min.css'); ?>
