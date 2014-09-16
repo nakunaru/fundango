5df07db82f9403cee4060c47c64c76d5e50d8e57
karamage <karamage.umauma@gmail.com>
2014-09-17 08:12:27 +0900

    show userlist in login page

--- fuel/app/views/scds/userlist.php
+++ fuel/app/views/scds/userlist.php
@@ -1,9 +1,12 @@
-<ul data-role="listview" data-filter="true" data-filter-placeholder="Search Users...">
-    <?php
-    echo '<li data-role="list-divider">ユーザランキング</li>';
-    foreach ($users as $data)
-    {
-        echo '<li>' . $data->screen_name .'</li>';
-    }
-    ?>
-</ul>
+
+<div role="main" class="ui-content">
+    <ul data-role="listview" data-filter="true" data-filter-placeholder="Search Users...">
+        <?php
+        echo '<li data-role="list-divider">ユーザランキング</li>';
+        foreach ($users as $data)
+        {
+            echo '<li>' . $data->screen_name .'</li>';
+        }
+        ?>
+    </ul>
+</div>
