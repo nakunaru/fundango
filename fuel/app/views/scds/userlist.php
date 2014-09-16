b841cbeea96c7b17cc5acbf1d55fe11e3db7a132
karamage <karamage.umauma@gmail.com>
2014-09-17 08:24:41 +0900

    show userlist in login page

--- fuel/app/views/scds/userlist.php
+++ fuel/app/views/scds/userlist.php
@@ -1,6 +1,6 @@
 
 <div role="main" class="ui-content">
-    <ol data-role="listview" data-inset="true" data-filter="true" data-filter-placeholder="Search Users...">
+    <ol data-role="listview" data-inset="true" >
         <?php
         echo '<li data-role="list-divider">ユーザ株価ランキング</li>';
         foreach ($users as $data)
