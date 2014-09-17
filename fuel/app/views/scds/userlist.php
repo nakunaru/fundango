1d5912e59d0f206c1e4ad9d0e59d056dd9eab8b5
karamage <karamage.umauma@gmail.com>
2014-09-18 08:12:11 +0900

    show userlist in login page

--- fuel/app/views/scds/userlist.php
+++ fuel/app/views/scds/userlist.php
@@ -5,7 +5,7 @@
         echo '<li data-role="list-divider">ユーザ株価ランキング</li>';
         foreach ($users as $data)
         {
-            echo '<li><a href="' . URI::create('user/view'); . '?screen_name=' . $data->screen_name .'"><img src="' . $data->avator . '"></img>' . '<div>' . $data->screen_name . '</div>' . '<p class="ui-li-count">' . $data->total_credit . 'd</p>' .'</a></li>';
+            echo '<li><a href="' . URI::create('user/view') . '?screen_name=' . $data->screen_name .'"><img src="' . $data->avator . '"></img>' . '<div>' . $data->screen_name . '</div>' . '<p class="ui-li-count">' . $data->total_credit . 'd</p>' .'</a></li>';
         }
         ?>
     </ol>
