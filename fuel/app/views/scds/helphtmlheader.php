b87fe59d9919ef49f6517689588ca55cae8557d5
karamage <karamage.umauma@gmail.com>
2014-09-23 13:45:40 +0900

    jquery fsvs and swipe file add

--- fuel/app/views/scds/helphtmlheader.php
+++ fuel/app/views/scds/helphtmlheader.php
@@ -1,6 +1,5 @@
 <meta charset="UTF-8" />
 <link rel="shortcut icon" href="<?php echo Asset::get_file('favicon.ico', 'img'); ?>" >
 <script type="text/javascript" src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
-<?php echo Asset::js('jquery.event.swipe.js'); ?>
 <?php echo Asset::js('fsvs.js'); ?>
 <?php echo Asset::js('sphelp.js'); ?>
