ae88aadb29dd7719512e88f8364f783eff16b463
karamage <karamage.umauma@gmail.com>
2014-10-01 08:32:52 +0900

    reply tweet update function add

--- fuel/app/classes/controller/deposit.php
+++ fuel/app/classes/controller/deposit.php
@@ -103,7 +103,7 @@ class Controller_Deposit extends Controller
         $message = Input::param('message');
         $to_image_url = Input::param('to_image_url');
         $from_image_url = Input::param('from_image_url');
-        $in_reoly_to_status_id = Input::param('in_reply_to_status_id');
+        $in_reply_to_status_id = Input::param('in_reply_to_status_id');
 
         //デポジット可能数が足りないならエラーにする
         if ($user->social_credit - $user->deposit_credit - $depositnum < 0) {
@@ -172,12 +172,12 @@ class Controller_Deposit extends Controller
         //$output = print_r($istweet,true);
         //Log::warning('istweet = ' . $output);
         if ($istweet === "on") {
-            if ($in_reoly_to_status_id == "") {
+            if ($in_reply_to_status_id == "") {
                 //twitter に投稿するやり方
                 $result = Timelinecommon::updatetimeline($to_user->screen_name, $depositnum);
             } else {
                 //twitter に投稿するやり方
-                $result = Timelinecommon::updatetimelinereply($to_user->screen_name, $depositnum, $in_reoly_to_status_id);
+                $result = Timelinecommon::updatetimelinereply($to_user->screen_name, $depositnum, $in_reply_to_status_id);
             }
         } else {
             //Log::warning('not tweet');
