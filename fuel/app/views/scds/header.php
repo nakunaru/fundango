eb7428762c3bf57fb3c27d5b0295cad43987970f
karamage <karamage.umauma@gmail.com>
2014-10-02 08:23:59 +0900

    odometer add

--- fuel/app/views/scds/header.php
+++ fuel/app/views/scds/header.php
@@ -1,6 +1,6 @@
 <div data-role="header" data-position="fixed">
     <!-- data-icon="gear" data-icon="friend" -->
     <a href="#" id="settingopenbtn">情報</a>
-    <h1><div id="odometer" class="odometer" credit="<?php echo $user['total_credit']; ?>">00000</div>d</h1>
+    <h1><div id="odometer" class="odometer" credit="<?php echo '' .(0 + $user['social_credit'] + $user['deposited_credit']) . 'd'; ?>">00000</div>d</h1>
     <a href="#" id="friendopenbtn">友達</a>
 </div>
