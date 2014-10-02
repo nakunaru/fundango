5949054d224932643199b13c169431b6e3a4a531
karamage <karamage.umauma@gmail.com>
2014-10-03 07:44:37 +0900

    user info odometer add

--- fuel/app/views/scds/userheader.php
+++ fuel/app/views/scds/userheader.php
@@ -1,8 +1,8 @@
 <div data-role="header">
     <h1>
         <?php echo '<div><img id="myaccountimg" src="'
-            . $user['avator'] . '"><div class="badge">'
-            . (0 + $user['social_credit']
-                + $user['deposited_credit']) . 'd</div></img>' . $user['screen_name'] . '</div>' ?>
+            . $user['avator'] . '">'
+                . '</img>' . $user['screen_name'] . '</div>' ?>
+        現値:<div id="odometer" class="odometer" credit="<?php echo '' .(0 + $user['social_credit'] + $user['deposited_credit']); ?>">00000</div>
     </h1>
 </div>
