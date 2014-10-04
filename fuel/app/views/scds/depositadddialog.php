2ffc8eb3afa4d111308a81c76e9f1aa10096bd72
karamage <karamage.umauma@gmail.com>
2014-10-04 11:14:10 +0900

    deposit dialog data-ajax=false

--- fuel/app/views/scds/depositadddialog.php
+++ fuel/app/views/scds/depositadddialog.php
@@ -5,7 +5,7 @@
     <div role="main" class="ui-content">
         <div id="depositaddscreenname"></div>
         <br>
-        <form action="<?php echo URI::create('deposit/add'); ?>" method="post" data-ajax=“false”>
+        <form action="<?php echo URI::create('deposit/add'); ?>" method="post" data-ajax="false">
             <input type="hidden" id="to_screen_name" name="to_screen_name" value="">
             <input type="hidden" id="to_tuserid" name="to_tuserid" value="">
             <input type="hidden" id="to_image_url" name="to_image_url" value="">
--- fuel/app/views/scds/depositdeldialog.php
+++ fuel/app/views/scds/depositdeldialog.php
@@ -4,7 +4,7 @@
     </div>
     <div role="main" class="ui-content">
         <div id="depositdelscreenname"></div>
-        <form action="<?php echo URI::create('deposit/del'); ?>" method="post" data-ajax=“false”>
+        <form action="<?php echo URI::create('deposit/del'); ?>" method="post" data-ajax="false">
             <input type="hidden" id="del_port4lio_id" name="del_port4lio_id" value="">
             <input type="hidden" id="del_to_screen_name" name="del_to_screen_name" value="">
             <input type="hidden" id="del_to_tuserid" name="del_to_tuserid" value="">
