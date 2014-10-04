1c6d9fc17a36f59fdb01aa805bec0a625849372f
karamage <karamage.umauma@gmail.com>
2014-10-04 20:49:19 +0900

    nikkeiheikin dango kabuka add

--- fuel/app/views/scds/login.php
+++ fuel/app/views/scds/login.php
@@ -34,10 +34,10 @@
       <center>
           <h1 class="fadeIn">Fundangoは、みんなが"上場"している社会を実現します</h1>
           <p class="description fadeIn">Fundangoの名前の由来は、「私は○○さんの"ファン"です」の"fun"と"dango"=団子とを掛けあわせた言葉です。<br>このサービスにログインするためにはTwitterアカウントが必要です。</p>
-          <h1>現在の日経平均団子株価</h1>
-          <div id="odometernikkei" class="odometer" >00000</div>d
+          <center><div><?php echo Asset::img('nyandango_128.png',array('id'=>'loginnyandango', 'class'=>'tossing'));  ?></div></center>
+          <h1>日経平均団子株価</h1>
+          <h1><div id="odometernikkei" class="odometer" >00000</div>d</h1>
       </center>
-      <center><div><?php echo Asset::img('nyandango_128.png',array('id'=>'loginnyandango', 'class'=>'tossing'));  ?></div></center>
     <!-- center><img id="logintitle"  src="../../images/title.jpg"></center -->
     <center><div><?php echo Asset::img('fundango_logo_mini.jpg',array('id'=>'logintitle', 'class'=>'hatch'));  ?></div></center>
     <div id="sociallogincontain" data-role="fieldcontain">
