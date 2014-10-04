a58748bdb270785d26a715b355c46605bd04ab4d
karamage <karamage.umauma@gmail.com>
2014-10-04 21:25:45 +0900

    nikkeiheikin dango kabuka add

--- public/assets/js/spinit.js
+++ public/assets/js/spinit.js
@@ -125,7 +125,7 @@ $(document).on( "pageinit", "#login", function( event ) {
     var creditlist = $('.total_credit');
     var total_credit = 0;
     for (var i=0; i<creditlist.length; i++) {
-        var credit = 0 + creditlist[i].attr('total_credit');
+        var credit = 0 + $(creditlist[i]).attr('total_credit');
         total_credit = total_credit + credit;
     }
     total_credit = total_credit / 20;
