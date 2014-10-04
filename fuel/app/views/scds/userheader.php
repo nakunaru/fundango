0e5d4915b13e9a911e472f69d576ca40241323ad
karamage <karamage.umauma@gmail.com>
2014-10-04 16:01:59 +0900

    user header description add

--- fuel/app/views/scds/userheader.php
+++ fuel/app/views/scds/userheader.php
@@ -3,6 +3,7 @@
         <?php echo '<img id="myaccountimg" src="'
             . $user['avator'] . '">'
                 . '</img><div>' . $user['screen_name'] . '</div>' ?>
-        現値:<div id="odometer2" class="odometer" credit="<?php echo '' .(0 + $user['social_credit'] + $user['deposited_credit']); ?>">00000</div>d
+        現値:<div id="odometer2" class="odometer" credit="<?php echo '' .(0 + $user['social_credit'] + $user['deposited_credit']); ?>">00000</div>d<br>
     </h1>
+    <p><?php echo $user['description'] ?></p>
 </div>
