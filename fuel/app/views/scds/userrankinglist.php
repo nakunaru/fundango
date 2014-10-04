f802bc48fe9950b3e1fde7d4f63f75ce0c292f1f
karamage <karamage.umauma@gmail.com>
2014-10-04 21:24:39 +0900

    nikkeiheikin dango kabuka add

--- fuel/app/views/scds/userrankinglist.php
+++ fuel/app/views/scds/userrankinglist.php
@@ -4,7 +4,7 @@
         echo '<li data-role="list-divider">ユーザ株価ランキング(上位20位)</li>';
         foreach ($users as $data)
         {
-            echo '<li><a href="' . URI::create('user/view') . '?screen_name=' . $data->screen_name .'" data-ajax="false"><img src="' . $data->avator . '"></img>' . '<div>' . $data->screen_name . '</div>' . '<p class="ui-li-count">' . $data->total_credit . 'd</p>' .'</a></li>';
+            echo '<li><a href="' . URI::create('user/view') . '?screen_name=' . $data->screen_name .'" data-ajax="false"><img src="' . $data->avator . '"></img>' . '<div>' . $data->screen_name . '</div>' . '<p class="ui-li-count total_credit" total_credit="' . $data->total_credit . '">' . $data->total_credit . 'd</p>' .'</a></li>';
         }
         ?>
 </ol>
--- public/assets/js/spinit.js
+++ public/assets/js/spinit.js
@@ -122,8 +122,15 @@ $(document).on( "pageinit", "#login", function( event ) {
     var test = "";
 
     //日経平均を出す
+    var creditlist = $('.total_credit');
+    var total_credit = 0;
+    for (var i=0; i<creditlist.length; i++) {
+        var credit = 0 + creditlist[i].attr('total_credit');
+        total_credit = total_credit + credit;
+    }
+    total_credit = total_credit / 20;
     setTimeout(function(){
-        odometernikkei.innerHTML = '12345';
+        odometernikkei.innerHTML = '' + total_credit;
     }, 1000);
 });
 
