80ac2518710ebdbdf6ba0942c9b9e205faaaf8ed
karamage <karamage.umauma@gmail.com>
2014-10-08 21:31:16 +0900

    userview data-ajax false

--- fuel/app/views/scds/userlist2.php
+++ fuel/app/views/scds/userlist2.php
@@ -4,7 +4,7 @@
         echo '<li data-role="list-divider">ユーザリスト</li>';
         foreach ($users as $data)
         {
-            echo '<li><a href="' . URI::create('user/view') . '?screen_name=' . $data->screen_name .'"><img src="' . $data->avator . '"></img>' . '<div>' . $data->screen_name . '</div>' . '<p class="ui-li-count">' . $data->total_credit . 'd</p>' .'</a></li>';
+            echo '<li><a data-ajax="false" href="' . URI::create('user/view') . '?screen_name=' . $data->screen_name .'"><img src="' . $data->avator . '"></img>' . '<div>' . $data->screen_name . '</div>' . '<p class="ui-li-count">' . $data->total_credit . 'd</p>' .'</a></li>';
         }
         ?>
     </ul>
