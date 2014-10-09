7e5d2616dc684471b303919dbf2d0a4ffe8b2301
karamage <karamage.umauma@gmail.com>
2014-10-10 07:47:37 +0900

    twitter login button inline

--- fuel/app/views/scds/login.php
+++ fuel/app/views/scds/login.php
@@ -41,7 +41,7 @@
     <div id="sociallogincontain" data-role="fieldcontain">
         <div data-role="fieldcontain">
             <!-- a href="home.html" data-icon="twittericon" data-role="button" id="tweetloginbtn">Twitterでログイン</a -->
-            <?php echo Html::anchor('twitterlogin/login', 'Twitterでログイン', array( 'data-role'=>'button','id'=>'tweetloginbtn', 'data-inset'=>'true', 'data-ajax'=>'false', 'class'=>'pulse')); ?>
+            <?php echo Html::anchor('twitterlogin/login', 'Twitterでログイン', array( 'data-role'=>'button','id'=>'tweetloginbtn', 'data-inline'=>'true', 'data-ajax'=>'false', 'class'=>'pulse')); ?>
             <!--a href="#" class="pullUp" data-icon="facebookicon" data-role="button" id="fbloginbtn">Facebookでログイン</a -->
             <br>
             <br>
