e8ef4c55c6e64153e3f4b308c08f375a4457b177
karamage <karamage.umauma@gmail.com>
2014-10-17 07:56:19 +0900

    dango color and board menu delete

--- fuel/app/views/scds/userinfo.php
+++ fuel/app/views/scds/userinfo.php
@@ -1,6 +1,6 @@
 <ul data-role="listview" >
     <li>ランキング <?php echo $rank['user_rank'] ?>/<?php echo $rank['all_users_count'] ?></li>
-    <li>dangoのカラー 青</li>
+    <!-- li>dangoのカラー 青</li -->
     <li>持ってるdango数<div class="ui-li-count"><?php echo $user['social_credit'] . 'd'; ?></div></li>
     <li>残dango<div id="account_enabled_deposit_credit" credit="<?php echo $user['social_credit'] - $user['deposit_credit']?>" class="ui-li-count"><?php echo $user['social_credit'] - $user['deposit_credit'] . 'd' ?></div></li>
     <li>デポった数<div class="ui-li-count"><?php echo $user['deposit_credit'] . 'd' ?></div></li>
