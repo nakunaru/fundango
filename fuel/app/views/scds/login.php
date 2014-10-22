a143fd92f458331da595ff54f0e65affe2716f87
karamage <karamage.umauma@gmail.com>
2014-10-22 21:57:55 +0900

    add facebook button

--- fuel/app/views/scds/login.php
+++ fuel/app/views/scds/login.php
@@ -72,7 +72,7 @@
   </div>
 
   <div data-role="footer" data-position="fixed" data-fullscreen="true">
-    <h3><div class="fb-like" expr:data-href="data:blog.url" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"/>Copyright 2014, Masaaki Kakimoto</h3>
+    <h3><div class="fb-like" expr:data-href="data:blog.url" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"/><iframe src="//www.facebook.com/plugins/follow?href=https%3A%2F%2Fwww.facebook.com%2Fkaramage.umauma&amp;layout=standard&amp;show_faces=true&amp;colorscheme=light&amp;width=450&amp;height=80" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:80px;" allowTransparency="true"></iframe></h3>
   </div>
 
 </div>
