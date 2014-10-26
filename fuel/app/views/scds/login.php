e3eb73edd58bbf01b0f5bce5ed62d54f83c5a62c
karamage <karamage.umauma@gmail.com>
2014-10-26 09:14:23 +0900

    login page update

--- fuel/app/views/scds/login.php
+++ fuel/app/views/scds/login.php
@@ -40,15 +40,12 @@
       <center>
           <h2 class="">Fundangoは、みんなが"上場"している社会を実現します</h2>
           <br>
-          <p class="description">名前の由来は、「私は○○さんの"ファン"です」の"fun"と"dango"=団子とを掛けあわせた言葉です。</p>
-          <p class="description">このサービスの目的は、<b>個人の信用を見える化する</b>ことです。 誰が誰に対して信用しているのか、それを数値やグラフとして表示します。</p>
-          <br>
-          <h3>現在の日経平均Dango株価</h3>
-          <h1><div id="odometernikkei" class="odometer" >0724545</div>d</h1>
       </center>
     <!-- center><img id="logintitle"  src="../../images/title.jpg"></center -->
     <!-- center><div><?php echo Asset::img('fundango_logo_mini.jpg',array('id'=>'logintitle', 'class'=>'hatch'));  ?></div></center -->
       <center>
+          <center><div><?php echo Asset::img('nyandango_128.png',array('id'=>'loginnyandango', 'class'=>'tossing'));  ?></div></center>
+          <br>
           <?php echo Html::anchor('twitterlogin/login', 'FundangoにTwitterでログインする!', array( 'data-role'=>'button','id'=>'tweetloginbtn', 'data-inline'=>'true', 'data-ajax'=>'false', 'class'=>'pulse')); ?>
       </center>
       <br>
@@ -57,11 +54,14 @@
             <!-- a href="home.html" data-icon="twittericon" data-role="button" id="tweetloginbtn">Twitterでログイン</a -->
             <!--a href="#" class="pullUp" data-icon="facebookicon" data-role="button" id="fbloginbtn">Facebookでログイン</a -->
             <center>
+                <p class="description">名前の由来は、「私は○○さんの"ファン"です」の"fun"と"dango"=団子とを掛けあわせた言葉です。</p>
+                <p class="description">このサービスの目的は、<b>個人の信用を見える化する</b>ことです。 誰が誰に対して信用しているのか、それを数値やグラフとして表示します。</p>
+                <br>
+                <h3>現在の日経平均Dango株価</h3>
+                <h1><div id="odometernikkei" class="odometer" >0724545</div>d</h1>
                 <p class="description">このサービスにログインするためにはTwitterアカウントが必要です。</p>
                 <a href="https://twitter.com/share" class="twitter-share-button">Tweet</a>
                 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-            <br>
-                <center><div><?php echo Asset::img('nyandango_128.png',array('id'=>'loginnyandango', 'class'=>'tossing'));  ?></div></center>
             <br>
             <p class="description fadeIn">このサービスは<a href="https://twitter.com/kara_mage" target="_blank">@kara_mage(柿本匡章)</a>が運営しています。<br>からまげについて詳しく知りたい方は下記に今すぐアクセス!</p>
             <a data-role="button" data-inline="true" data-mini="true" href="http://www.karamage.com/" target="_blank">http://www.karamage.com</a>
